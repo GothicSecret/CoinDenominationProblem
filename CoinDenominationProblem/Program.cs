4aebe7ed84ec65ecb121a4c4880805945b37b7f9
xivSolutions <xivSolutions@gmail.com>
2018-02-22 08:04:56 -0700

    better data structure

--- CoinDenominationProblem/Program.cs
+++ CoinDenominationProblem/Program.cs
@@ -2,32 +2,52 @@
 using System.Collections.Generic;
 using System.Linq;
 
-namespace CoinDenomination
+namespace CoinDenominationProblem
 {
+    public class CoinPurse
+    {
+        public CoinPurse()
+        {
+            Coins = new Dictionary<decimal, int>()
+            {
+                {.04M, 0},
+                {.05M, 0},
+                {.07M, 0}
+            };
+        }
+        public Dictionary<decimal, int> Coins { get; private set; }
+
+        public decimal GetTotal()
+        {
+            decimal result = 0M;
+            foreach (var kvp in Coins)
+            {
+                result += kvp.Key*kvp.Value;
+            }
+            return result;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            decimal cost = 31.00M;
-            decimal maxCost = 31.10M;
+            decimal cost = .07M;
+            decimal maxCost = .31M;
 
             do
             {
                 var coins = FindCoins(cost);
-                var four = coins.Count(x => x == .04M);
-                var five = coins.Count(x => x == .05M);
-                var seven = coins.Count(x => x == .07M);
-
-                Console.WriteLine($"Coint total amount: {coins.Sum()}: {seven} seven cent coins, {five} five cent coins, {four} four cent coins");
+                Console.WriteLine($"Coint total amount: {coins.GetTotal()}: {coins.Coins[.07M]} seven cent coins, {coins.Coins[.05M]} five cent coins, {coins.Coins[.04M]} four cent coins");
                 cost += .01M;
             } while (cost <= maxCost);
             Console.Read();
         }
 
-        public static List<decimal> FindCoins(decimal cost)
+        public static CoinPurse FindCoins(decimal cost)
         {
             int theIntCost = decimal.ToInt32(cost * 100);
-            var coins = new List<decimal>();
+            var coinPurse = new CoinPurse();
             int remainder = 0;
 
             while (theIntCost > 0)
@@ -35,37 +55,41 @@ namespace CoinDenomination
                 switch (theIntCost % 7)
                 {
                     case 0:
-                        coins.Add(.07M);
-                        theIntCost -= 7;
+                        int remainingSevens = theIntCost/7;
+                        coinPurse.Coins[.07M] += remainingSevens;
+                        theIntCost -= remainingSevens * 7;
                         break;
                     case 5:
-                        coins.Add(.05M);
+                        coinPurse.Coins[.05M] += 1;
                         theIntCost -= 5;
                         break;
                     case 4:
-                        coins.Add(.04M);
+                        coinPurse.Coins[.04M] += 1;
                         theIntCost -= 4;
                         break;
                     default:
                         remainder += theIntCost%7;
                         if (remainder == 4 || remainder == 5 || remainder == 7)
                         {
-                            coins.Add(remainder*.01M);
+                            coinPurse.Coins[remainder*.01M] += 1;
                             theIntCost -= remainder;
                             remainder = 0;
                         }
                         if (remainder > 7)
                         {
-                            var remainderCoins = FindCoins(remainder * .01M);
-                            coins.AddRange(remainderCoins);
-                            theIntCost -= decimal.ToInt32(remainderCoins.Sum() * 100);
+                            var remainderPurse = FindCoins(remainder * .01M);
+                            foreach (var denomination in remainderPurse.Coins)
+                            {
+                                coinPurse.Coins[denomination.Key] += denomination.Value;
+                            }
+                            theIntCost -= decimal.ToInt32(remainderPurse.GetTotal() * 100);
                             ;
                             remainder = 0;
                         }
                         break;
                 }
             }
-            return coins;
+            return coinPurse;
         }
     }
 }
