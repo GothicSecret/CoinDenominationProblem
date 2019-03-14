917d6a911b3869c59120a5feddfa54c31b43d9bb
Maksym Podhornyi <MPodhornyi@spherasolutions.com>
2019-03-14 14:32:31 +0200

    easier way

--- CoinDenominationProblem/Program.cs
+++ CoinDenominationProblem/Program.cs
@@ -6,29 +6,19 @@ namespace CoinDenominationProblem
 {
     public class CoinPurse
     {
-        public CoinPurse()
-        {
-            Coins = new Dictionary<decimal, int>()
-            {
-                {.04M, 0},
-                {.05M, 0},
-                {.07M, 0}
-            };
-        }
-        public Dictionary<decimal, int> Coins { get; private set; }
+        public int Coins7 { get; set; }
+
+        public int Coins5 { get; set; }
+
+        public int Coins4 { get; set; }
 
         public decimal GetTotal()
         {
-            decimal result = 0M;
-            foreach (var kvp in Coins)
-            {
-                result += kvp.Key*kvp.Value;
-            }
-            return result;
+            return Coins7 * 7 + Coins5 * 5 + Coins4 * 4;
         }
     }
 
-    class Program
+    internal class Program
     {
         static void Main(string[] args)
         {
@@ -38,7 +28,7 @@ namespace CoinDenominationProblem
             do
             {
                 var coins = FindCoins(cost);
-                Console.WriteLine($"Coint total amount: {coins.GetTotal()}: {coins.Coins[.07M]} seven cent coins, {coins.Coins[.05M]} five cent coins, {coins.Coins[.04M]} four cent coins");
+                Console.WriteLine($"Coint total amount: {coins.GetTotal()}: {coins.Coins7} seven cent coins, {coins.Coins5} five cent coins, {coins.Coins4} four cent coins");
                 cost += .01M;
             } while (cost <= maxCost);
             Console.Read();
@@ -47,47 +37,42 @@ namespace CoinDenominationProblem
         public static CoinPurse FindCoins(decimal cost)
         {
             int theIntCost = decimal.ToInt32(cost * 100);
-            var coinPurse = new CoinPurse();
-            int remainder = 0;
-
-            while (theIntCost > 0)
+            var coinPurse = new CoinPurse()
             {
-                switch (theIntCost % 7)
-                {
-                    case 0:
-                        int remainingSevens = theIntCost/7;
-                        coinPurse.Coins[.07M] += remainingSevens;
-                        theIntCost -= remainingSevens * 7;
-                        break;
-                    case 5:
-                        coinPurse.Coins[.05M] += 1;
-                        theIntCost -= 5;
-                        break;
-                    case 4:
-                        coinPurse.Coins[.04M] += 1;
-                        theIntCost -= 4;
-                        break;
-                    default:
-                        remainder += theIntCost%7;
-                        if (remainder == 4 || remainder == 5 || remainder == 7)
-                        {
-                            coinPurse.Coins[remainder*.01M] += 1;
-                            theIntCost -= remainder;
-                            remainder = 0;
-                        }
-                        if (remainder > 7)
-                        {
-                            var remainderPurse = FindCoins(remainder * .01M);
-                            foreach (var denomination in remainderPurse.Coins)
-                            {
-                                coinPurse.Coins[denomination.Key] += denomination.Value;
-                            }
-                            theIntCost -= decimal.ToInt32(remainderPurse.GetTotal() * 100);
-                            ;
-                            remainder = 0;
-                        }
-                        break;
-                }
+                Coins7 = theIntCost / 7
+            };
+            int remainder = theIntCost - coinPurse.Coins7 * 7;
+            switch (remainder)
+            {
+                case 1:// -7 so remains 1 + 7 = 8. Add 4 + 4
+                    coinPurse.Coins7--;
+                    coinPurse.Coins4 += 2;
+                    break;
+
+                case 2:// -7 so remains 2 + 7 = 9. Add 4 + 5
+                    coinPurse.Coins7--;
+                    coinPurse.Coins5++;
+                    coinPurse.Coins4++;
+                    break;
+
+                case 3:// -7 so remains 3 + 7 = 10. Add 5 + 5
+                    coinPurse.Coins7--;
+                    coinPurse.Coins5 += 2;
+                    break;
+
+                case 4:// just add 4
+                    coinPurse.Coins4++;
+                    break;
+
+                case 5:// just add 5
+                    coinPurse.Coins5++;
+                    break;
+
+                case 6:// -7 so remains 6 + 7 = 13. Add 4 + 4 + 5
+                    coinPurse.Coins7--;
+                    coinPurse.Coins4 += 2;
+                    coinPurse.Coins5++;
+                    break;
             }
             return coinPurse;
         }
